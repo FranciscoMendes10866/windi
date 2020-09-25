93a5ef50ee3749aa8f3256d712446f48719c672c
FranciscoMendes10866 <avizinhadochico@gmail.com>
2020-09-25 23:18:59 +0100

    intalled vuex persit state

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -20,7 +20,7 @@ export default {
   css: [],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: ['~/plugins/clipboard'],
+  plugins: ['~/plugins/clipboard', '~/plugins/persist'],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
